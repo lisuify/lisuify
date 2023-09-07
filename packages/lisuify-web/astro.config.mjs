0387ac6861a3823509e28da1c98281a7484b131e
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-09-07 18:28:18 +0700

    chore: initial pnopm workspace and astro peoject

---
+++ packages/lisuify-web/astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+import serviceWorker from "astrojs-service-worker";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+  compressHTML: true,
+  integrations: [tailwind(), svelte(), serviceWorker()],
+  vite: {
+    plugins: [],
+  },
+});
