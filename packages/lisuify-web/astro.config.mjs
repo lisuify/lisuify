d454a0c07b20b623f623f48ece989828555e087e
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-09-13 05:05:24 +0700

    feat: remove astro cloudflare

--- packages/lisuify-web/astro.config.mjs
+++ packages/lisuify-web/astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import serviceWorker from "astrojs-service-worker";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  compressHTML: true,
+  output: "static",
+  compressHTML: false,
   integrations: [tailwind(), svelte(), serviceWorker()],
   vite: {
     plugins: [],
   },
-  adapter: cloudflare(),
 });
