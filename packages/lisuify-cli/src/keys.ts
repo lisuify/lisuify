2ae6ca720aaf4c54ce45f5eee14afe4adac5032a
aankor <aankor@marinade.finance>
2023-09-12 10:06:06 +0200

    Update address

--- packages/lisuify-cli/src/keys.ts
+++ packages/lisuify-cli/src/keys.ts
@@ -1,5 +1,5 @@
 export const lisuifyId =
-  '0xc1f2f6e9bcf988a4a5b9af19e6f586d2ec1d55c9d0795bd2a73da13e61a1201a';
+  '0x1eb996fd9b0ec0f43d5b6a6e10ad1f516fb630a3eb76278cb2286db67b0e9775';
 
 export const stakePoolId =
   '0x5e52e2ab4ac937267a091af48146c1a6bdeeca9232c492eac1b3858bd3dd3b90';
