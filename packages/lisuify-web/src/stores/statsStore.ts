979f6d1ae87d6f80e43b7043ec2e8ccc5bdaabf1
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-09-15 06:26:35 +0700

    feat: add apy show

--- packages/lisuify-web/src/stores/statsStore.ts
+++ packages/lisuify-web/src/stores/statsStore.ts
@@ -2,10 +2,12 @@ import { atom } from "nanostores";
 import type { Stats } from "../types";
 import { getCurrentSuiBalance, getLiSUIRatio } from "../client/lisuify";
 import { log } from "../utils";
+import { getValidatorAPY } from "../client/sui";
 
 const defaulStats: Stats = {
   totalSuiStaking: BigInt(0),
   liSuiRatio: 1,
+  validatorApy: 5.5,
 };
 
 export const statsAtom = atom<Stats>(defaulStats);
@@ -13,13 +15,23 @@ export const statsAtom = atom<Stats>(defaulStats);
 const initialStats = async () => {
   const totalSuiBalance = await getCurrentSuiBalance();
   const liSuiRatio = await getLiSUIRatio();
+  const validatorApy = await getValidatorAPY();
 
   log("totalSuiBalance", totalSuiBalance);
   log("liSuiRatio", liSuiRatio);
+  log("validatorApy", validatorApy);
+  const averageApy =
+    (validatorApy.apys.reduce((sum, a) => {
+      return sum + a.apy;
+    }, 0) /
+      validatorApy.apys.length) *
+    100;
+  log("averageApy", averageApy);
 
   statsAtom.set({
     totalSuiStaking: totalSuiBalance,
     liSuiRatio: liSuiRatio,
+    validatorApy: averageApy,
   });
 };
 
