f5aafa4c64c56dfb3abc032f7395d8d8cb813d10
aankor <aankor@marinade.finance>
2023-09-14 23:13:46 +0200

    Fix update protection

--- packages/lisuify-cli/src/keys.ts
+++ packages/lisuify-cli/src/keys.ts
@@ -2,7 +2,7 @@ export const originalLisuifyId =
   '0x69a77dd93696e2b247b731b48906f6687656cfe27e6ec03328ef099e248e333b';
 
 export const lisuifyId =
-  '0x69a77dd93696e2b247b731b48906f6687656cfe27e6ec03328ef099e248e333b';
+  '0xbecd91a6397b53413781a658058dc7f5fb0f464eea01d6fb47639a9b4555dbae';
 
 export const stakePoolId =
   '0x4ba1b90c9bd93b0bd0d2116a0baa8faea4d4e944015678d577ff5b3f711f2918';
