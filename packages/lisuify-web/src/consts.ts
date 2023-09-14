331a51c26a78c46f926e9918478a00b83b34d169
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-09-15 04:03:11 +0700

    fix: dark mode and move menu to const
    - fix dark mode
    - move menu to const

--- packages/lisuify-web/src/consts.ts
+++ packages/lisuify-web/src/consts.ts
@@ -15,11 +15,23 @@ export const lisuifyId = import.meta.env.PUBLIC_LISUIFY_ID;
 
 export const stakePoolId = import.meta.env.PUBLIC_STAKE_POOL_ID;
 
-export const MENU = {
+export const SIDE_MENU = {
   "Getting Started": [
     "/docs/getting-started/what-is-lisuify",
     "/docs/getting-started/what-is-lisui",
     "/docs/getting-started/faq",
   ],
   LiSuify: ["/docs/lisuify/introduction", "/docs/lisuify/liquid-staking"],
+};
+
+export const NAVIGATION_MENU = {
+  "Getting Started": {
+    "What is LiSuify?": "/docs/getting-started/what-is-lisuify",
+    "What is liSUI?": "/docs/getting-started/what-is-lisui",
+    FAQ: "/docs/getting-started/faq",
+  },
+  Documents: {
+    Introduction: "/docs/lisuify/introduction",
+    "Liquid Staking": "/docs/lisuify/liquid-staking",
+  },
 };
