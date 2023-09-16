5e996a49f7fec11e97ad040a782f917b02294f03
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-09-16 07:24:16 +0700

    docs: add what is lisuify document, liquid staking, and refactor docs
    - add `what is lisuify?` document
    - add liquid staking document
    - refactor other docs

--- packages/lisuify-web/src/consts.ts
+++ packages/lisuify-web/src/consts.ts
@@ -16,18 +16,24 @@ export const stakePoolId = import.meta.env.PUBLIC_STAKE_POOL_ID;
 export const SIDE_MENU = {
   "Getting Started": [
     "/docs/getting-started/what-is-lisuify",
-    "/docs/getting-started/faq",
+    "/docs/getting-started/liquid-staking",
+    // "/docs/getting-started/faq",
   ],
-  LiSuify: ["/docs/lisuify/introduction", "/docs/lisuify/liquid-staking"],
+  LiSuify: [
+    "/docs/lisuify/whitepaper",
+    "/docs/lisuify/api-reference",
+    //
+  ],
 };
 
 export const NAVIGATION_MENU = {
   "Getting Started": {
     "What is LiSuify?": "/docs/getting-started/what-is-lisuify",
-    FAQ: "/docs/getting-started/faq",
+    "Liquid Staking": "/docs/getting-started/liquid-staking",
+    // FAQ: "/docs/getting-started/faq",
   },
   Documents: {
-    Introduction: "/docs/lisuify/introduction",
-    "Liquid Staking": "/docs/lisuify/liquid-staking",
+    Whitepaper: "/docs/lisuify/whitepaper",
+    "API Reference": "/docs/lisuify/api-reference",
   },
 };
